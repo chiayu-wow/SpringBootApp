bc2e74428789da862160d1a6912e943d49d79ec3
Yoyo <yoyowamg1012@gmail.com>
2025-04-29 17:35:47 -0500

    Initial Spring Boot commit

---
+++ src/main/java/com/test/FirstApp/Controller/PageController.java
@@ -0,0 +1,14 @@
+package com.test.FirstApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // Looks for home.html inside templates/
+    }
+}
+
